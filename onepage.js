c28ea765e7d51e8ec839d6933edd4cb35a311450
Brett Chisholm <brett.chisholm@propcom.co.uk>
2015-09-17 11:41:44 +0100

    [bugfix-analytics] - set correct nav link and remove viewport dependance

--- onepage.js
+++ onepage.js
@@ -1,163 +1,193 @@
 
-	// constructor
+// constructor
 
-	var onepage = function($page, $link, $offset) {
+var onepage = function($page, $link, $offset) {
 
-		this.page = $page;
-		this.pageClass = this.page.attr('class').split(' ')[0];
-		this.link = $link;
-		this.offset = $offset; 
-		this.init();
+    this.page = $page;
+    this.pageClass = this.page.attr('class').split(' ')[0];
+    this.link = $link;
+    this.offset = $offset;
+    this.init();
 
-		this.currentPage;
+    this.currentPage;
+    scrollFunc = true;
 
-	}
+}
 
-	onepage.prototype = {
+onepage.prototype = {
 
-		init : function() {
+    init : function() {
 
-			var loadedPage = window.location.pathname.substr(1);
+        var loadedPage = window.location.pathname.substr(1);
 
-			if (loadedPage == '') {
-				loadedPage = 'index';
+        if (loadedPage == '') {
+            loadedPage = 'index';
+        }
+
+        // register events
+
+        this.register_events();
+
+        // on load scroll to correct page
+
+        $(window).scrollTop($('#'+loadedPage).offset().top - this.offset);
+
+    },
+
+    register_events : function() {
+
+        // obtain a reference
+
+        var self = this,
+            scrollEnd;
+
+        $(this.link).on('click', this.scrollIt);
+
+        // on scroll update url and push to ga
+
+        $(window).on('scroll', function(){
+
+        	if (scrollFunc == true) {
+            	window.requestAnimationFrame(scrollHandler);
+            }
+
+        });
+
+        function scrollHandler() {
+
+        	console.log('in the scroll handler');
+
+            $('html, body').css({
+                'pointer-events': 'none'
+            });
+
+            if(scrollEnd) {
+
+                clearTimeout(scrollEnd);
+            }
+
+            scrollEnd = setTimeout(function(){
+
+                self.switchActive();
+
+                $('html, body').css({
+                    'pointer-events': 'auto'
+                });
+
+
+            }, 200);
+        }
+
+    },
+
+    scrollIt : function(e) {
+
+        e.preventDefault();
+
+        scrollFunc = false;
+
+        var self = onepage,
+            link = $(this).attr('href').replace('/', '');
+
+        self.translate(link);
+
+        setTimeout(function(){
+
+            self.urlPush(link);
+            self.linkActive(link);
+            scrollFunc = true;
+
+        }, 800);
+
+    },
+
+    translate : function(target) {
+
+        var self = this;
+
+        if (!target) {
+            target = 'index';
+        }
+
+        var scrollTo = ($('#'+target).offset().top - self.offset) - $('header').height();
+
+        $('html, body').animate({
+
+            scrollTop: scrollTo
+
+        }, 500);
+
+    },
+
+    switchActive : function() {
+
+        var pageId,
+        	self = this;
+
+		for (var i = this.page.length - 1; i >= 0; i--) {
+
+			var content = $(this.page[i]);
+
+			if ((content.offset().top - self.offset < $(window).scrollTop() ) && ( content.offset().top + content.outerHeight() - self.offset > $(window).scrollTop())) {
+
+				pageId = $(content).attr('id');
+
 			}
 
-			// register events
+		};        	
 
-			this.register_events();
+        this.urlPush(pageId);
 
-			// on load scroll to correct page
+        this.gaPush(pageId);
 
-			$(window).scrollTop($('#'+loadedPage).offset().top - this.offset);
+        this.linkActive(pageId);
 
-		},
+    },
 
-		register_events : function() {
+    linkActive : function(pageId) {
 
-			// obtain a reference
+    	console.log(pageId);
 
-			var self = this,
-				scrollEnd;
+        for (var i = this.link.length - 1; i >= 0; i--) {
 
-			$(this.link).on('click', this.scrollIt);
-				
-			// on scroll update url and push to ga
+            if ($(this.link[i]).attr('href') == pageId) {
 
-			$(window).on('scroll', function(){ 
+                $(this.link[i]).addClass('active');
 
-				window.requestAnimationFrame(scrollHandler);
+            } else {
 
-			});
+                $(this.link[i]).removeClass('active');
 
-			function scrollHandler() {
+            }
 
-				$('html, body').css({
-					'pointer-events': 'none'				
-				});
+        }
 
-				if(scrollEnd) {
+    },
 
-				    clearTimeout(scrollEnd);  
-				}
+    urlPush : function(page) {
 
-				scrollEnd = setTimeout(function(){
+        if (history.pushState) {
+            history.pushState('', '', page);
+        }
 
-				  	self.switchActive();
+    },
 
-					$('html, body').css({
-						'pointer-events': 'initial'
-					});
+    gaPush : function(page) {
 
+        var ga_page = page;
 
-				}, 200);
-			}
+        if(ga_page === this.currentPage) return;
 
-		},
+        this.currentPage = ga_page;
 
-		scrollIt : function(e) {
+        if (ga_page == 'index') {
+            ga_page = '';
+        }
 
-			e.preventDefault();
+        if (ga_page != undefined) {
+            ga('send', 'pageview', "/"+ga_page);
+        }
 
-			var self = onepage,
-				link = $(this).attr('href').replace('/', '');  
+    }
 
-			self.translate(link);
-
-			setTimeout(function(){
-			  
-				self.urlPush(link);
-
-			}, 800);
-
-		},
-
-		translate : function(target) {
-
-			var self = this;
-
-			if (!target) {
-				target = 'index';
-			}
-
-			$('html, body').animate({
-
-				scrollTop: $('#'+target).offset().top - self.offset  
-
-			}, 500, function(){
-				  
-				self.switchActive();
-
-			});
-
-		},
-
-		switchActive : function() {
-
-			var pageId = $('.'+this.pageClass+':in-viewport').attr('id');
-
-			this.urlPush(pageId);
-
-			this.gaPush(pageId);
-
-			for (var i = this.link.length - 1; i >= 0; i--) {
-
-				if ($(this.link[i]).attr('href') == pageId) {
-
-					$(this.link[i]).addClass('active');
-
-				} else {
-
-					$(this.link[i]).removeClass('active');				
-
-				}
-
-			}
-
-		},
-
-		urlPush : function(page) {
-
-			if (history.pushState) {
-				history.pushState('', '', page);
-			}
-
-		},
-
-		gaPush : function(page) {
-
-			var ga_page = page;
-
-			if(ga_page === this.currentPage) return;
-
-			this.currentPage = ga_page;
-
-			if (ga_page == 'index') {
-				ga_page = '';
-			}
-
-			ga('send', 'pageview', "/"+ga_page);
-
-		}
-
-	}
+}
